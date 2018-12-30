740efc8254f5fd4e1b8abd01053b19d7fa3e50b4
patrick.kelly <patrick.kelly@eigentech.com>
2018-12-30 14:46:52 -0500

    return sum and print function

--- python_solutions/problem_2.py
+++ python_solutions/problem_2.py
@@ -14,6 +14,7 @@ def get_even_fibonacci_sum(max_fib_val):
             totalSum += indexSum
         index1 = index2
         index2 = indexSum
-    return print(totalSum)
+    return totalSum
 
-get_even_fibonacci_sum(4000000)
+if __name__ == "__main__":
+	print(get_even_fibonacci_sum(4000000))
